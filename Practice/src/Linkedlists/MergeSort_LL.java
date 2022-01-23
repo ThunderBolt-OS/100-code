ff0d3d3dc12b4a1fd80464e2d7ab7b950c4c6319
ThunderBolt-OS <soham.bhoir@somaiya.edu>
2022-01-24 00:23:28 +0530

    day 17

---
+++ Practice/src/Linkedlists/MergeSort_LL.java
@@ -0,0 +1,57 @@
+package Linkedlists;
+
+import java.util.List;
+
+public class MergeSort_LL {
+    public static void main(String[] args) {
+
+    }
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    public ListNode sortList(ListNode head) {
+
+    }
+    ListNode merge(ListNode list1, ListNode list2){
+        ListNode dummyHead = new ListNode();
+        ListNode tail = dummyHead;
+
+        while (list1 != null && list2 != null){
+            if (list1.val < list2.val){
+                tail.next = list1;
+                list1 = list1.next;
+                tail = tail.next;
+            }else {
+                tail.next = list2;
+                list2 =list2.next;
+                tail = tail.next;
+            }
+        }
+        tail.next = (list1 != null) ? list1 : list2;
+        return dummyHead.next;
+    }
+    ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+}
