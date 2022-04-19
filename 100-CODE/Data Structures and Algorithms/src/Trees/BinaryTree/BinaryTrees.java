8c9a3a71fb1005cc18058a24094fd09b0b4b6398
ThunderBolt-OS <soham.bhoir@somaiya.edu>
2022-04-19 23:03:39 +0530

    learned: stacks & Queues

--- 100-CODE/Data Structures and Algorithms/src/Trees/BinaryTree/BinaryTrees.java
+++ 100-CODE/Data Structures and Algorithms/src/Trees/BinaryTree/BinaryTrees.java
@@ -186,31 +186,31 @@ public class BinaryTrees {
 //        return new TreeInfo(myHeight, myDiam);
 //    }
 
-    public boolean isIdentical(TreeNode root,TreeNode subRoot){
-        if(subRoot == null && root == null){
-            return true;
-        }
-        if(root == null || subRoot == null){
-            return false;
-        }
-        if(root.val == subRoot.val){
-            return isIdentical(root.left, subRoot.left) && isIdentical(root.right, subRoot.right);
-        }
-        return false;
-    }
-
-    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if(subRoot == null){
-            return true;
-        }
-        if(root == null){
-            return false;
-        }
-        if(isIdentical(root, subRoot)){
-            return true;
-        }
-        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
-    }
+//    public boolean isIdentical(TreeNode root,TreeNode subRoot){
+//        if(subRoot == null && root == null){
+//            return true;
+//        }
+//        if(root == null || subRoot == null){
+//            return false;
+//        }
+//        if(root == subRoot.val){
+//            return isIdentical(root.left, subRoot.left) && isIdentical(root.right, subRoot.right);
+//        }
+//        return false;
+//    }
+//
+//    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+//        if(subRoot == null){
+//            return true;
+//        }
+//        if(root == null){
+//            return false;
+//        }
+//        if(isIdentical(root, subRoot)){
+//            return true;
+//        }
+//        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+//    }
 
 
 
